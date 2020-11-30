c4106270a824ffbbb863c1b8d1b7db71404a7ee7
Cristobal Guerrero <cguerrerog@uc.cl>
2020-11-29 22:11:29 -0300

    avance inicial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,28 @@
 class ApplicationController < ActionController::Base
 
     def simulation
-        if params[:velocidad]
-            @velocidad = params[:velocidad]
+        if params[:velocidad_entrada]
+            @velocidad_entrada = params[:velocidad_entrada]
         else
-            @velocidad = 0
+            @velocidad_entrada = 0
         end
-        @coeficientes = [@velocidad]
+        if params[:velocidad_salida]
+            @velocidad_salida = params[:velocidad_salida]
+        else
+            @velocidad_salida = 0
+        end
+        if params[:presion_entrada]
+            @presion_entrada = params[:presion_entrada]
+        else
+            @presion_entrada = 0
+        end
+        if params[:presion_salida]
+            @presion_salida = params[:presion_salida]
+        else
+            @presion_salida = 0
+        end
+
+        @coeficientes = [@velocidad_entrada, @velocidad_salida, @presion_entrada, @presion_salida]
         respond_to do |format|
             format.html
             format.js
